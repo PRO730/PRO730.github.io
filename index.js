7d4ebc882cde7646293983e5b292103be3d91e7b
Luke Promise <lukepromise01@gmail.com>
2023-12-30 00:53:09 -0800

    improved events and add showpage(id)

--- index.js
+++ index.js
@@ -8,41 +8,16 @@ function show(id) {
   document.getElementById(id).style.display = "block";
 }
 
-function showHomepage() {
+function showpage(id) {
   hide(activepage);
-  show("Home");
-  activepage = "Home";
+  show(id);
+  activepage = id;
 }
 
-function showSkillspage() {
-  hide(activepage);
-  show("Skills");
-  activepage = "Skills";
-}
+showpage(activepage);
 
-function showProjectpage() {
-  hide(activepage);
-  show("Projects");
-  activepage = "Projects";
-}
-
-function showLanguagespage() {
-  hide(activepage);
-  show("Languages");
-  activepage = "Languages";
-}
-
-// showHomepage();
-showSkillspage();
-
-var homelink = document.querySelectorAll("#top-menu-bar a")[0];
-homelink.addEventListener("click", showHomepage);
-
-var skillslink = document.querySelectorAll("#top-menu-bar a")[1];
-skillslink.addEventListener("click", showSkillspage);
-
-var projectlink = document.querySelectorAll("#top-menu-bar a")[2];
-projectlink.addEventListener("click", showProjectpage);
-
-var Languageslink = document.querySelectorAll("#top-menu-bar a")[3];
-Languageslink.addEventListener("click", showLanguagespage);
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  var id = e.target.dataset.page;
+  console.info("click on menu-bar", id);
+  showpage(id);
+});
